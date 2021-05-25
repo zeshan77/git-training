63d514aeadafee464c3b7e64d75f213a3525bbc6
Muhammad Essa <iMuhammadessa@gmail.com>
2021-05-25 17:27:45 +0500

    Modified index.html, brought images and edited readme file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # git-training
-All about git training
+
+> All about git training
+
+#
+
+# @Kodecademy
+
+> We're a tech academy that teaches code to fresh graduates in a sense to prepare them for industry.
+
+#
+
+# Training glimpse
+
+![Git-training](images/git-training.jpg)
+
+> Academy site : https://kodecademy.com/
--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-hello world
+<!Doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="icon" href="images/KodeCademy.png" type="image/favicon">
+    <title>Git Training</title>
+
+  </head>
+  <body class="d-flex h-100 text-center text-white bg-dark">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+      <h3>Git training</h3>
+  </header>
+
+  <main class="px-3">
+    <h1>Git training by @kodecademy</h1>
+    <p class="lead">A tech academy that teaches code to fresh graduates in a sense to prepare them for an industry.</p>
+    <p class="lead">
+      <a href="https://kodecademy.com/" class="btn btn-lg btn-secondary fw-bold border-white bg-dark">Learn more</a>
+    </p>
+  </main>
+
+  <footer class="mt-auto text-white-50">
+    <p>Contributed by <a href="https://twitter.com/imuhammadessa" class="text-white">Muhammad Essa</a></p>
+  </footer>
+</div>
+
+
+    
+  </body>
+</html>
